42e27f57c27313ecb022a2a4b8d1df7f519dd71d
Nicholas Marozick <marozicn@flip2.engr.oregonstate.edu>
2019-07-28 14:23:56 -0700

    added script to validate that .txt files have valid input data

---
+++ validateTxt.py
@@ -0,0 +1,34 @@
+if __name__ == '__main__':
+
+    with open("test_coords.txt", "r") as f:
+        n=0
+        for l in f:
+            n+=1
+            print("Line Number: ", n)
+            #line = f.readline().strip()
+            #print(line)
+            line=l.strip()
+
+            line_size = [x for x in line.split()]
+            count=(len(line_size))
+            print(line_size)
+            print(count)
+            # if line size is greater than 3
+
+            if count>3 or count<3:
+                    print("Error, line is more than 3 coordinates\n")
+
+            # if non int data
+
+            for i in line_size:
+                numPeriods=0
+                for j in range(len(i)):
+                    #print(i[j])
+                    if i[j]!='1' and i[j]!='2' and i[j]!='3' and i[j]!='4' and i[j]!='5' and i[j]!='6' and i[j]!='7' and i[j]!='8' and i[j]!='9' and i[j]!='0' and i[j]!='.':
+                        print("invalid character input")
+                    if i[j]=='.':
+                        numPeriods+=1
+                    if numPeriods>1:
+                        print("number sequence provided is not a float")
+                #print('\n')
+                #print("num Periods: ", numPeriods)
